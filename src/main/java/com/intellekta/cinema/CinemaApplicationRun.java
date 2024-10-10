e04e6d39829ab45907f349566adf0ab7bfa33c41
Korablev Danil <ikorablev@gmail.com>
2024-10-10 22:40:25 +0500

    add getMoviesWatchedCount usage example to CinemaApplicationRun

--- src/main/java/com/intellekta/cinema/CinemaApplicationRun.java
+++ src/main/java/com/intellekta/cinema/CinemaApplicationRun.java
@@ -5,15 +5,30 @@ import java.util.List;
 
 public class CinemaApplicationRun {
     public static void main(String[] args) {
+        Cinema movie1 = new Cinema("The Lord of the Rings", "Fantasy", 3.5);
+        Cinema movie2 = new Cinema("Inception", "Science Fiction", 2.8);
+        Cinema movie3 = new Cinema("The Matrix", "Action", 2.3);
+
+        List<Cinema> user1Movies = Arrays.asList(movie1, movie2);
+        List<Cinema> user2Movies = Arrays.asList(movie2, movie3);
+        List<Cinema> user3Movies = Arrays.asList(movie1);
+        List<Cinema> user4Movies = Arrays.asList(movie1, movie2, movie3);
+        List<Cinema> user5Movies = Arrays.asList(movie3);
+
         List<Viewer> viewers = Arrays.asList(
-                new Viewer("User1", 20, 10),
-                new Viewer("User2", 25, 15),
-                new Viewer("User3", 30, 8),
-                new Viewer("User4", 22, 12),
-                new Viewer("User5", 27, 9)
+                new Viewer("User1", 20, user1Movies),
+                new Viewer("User2", 25, user2Movies),
+                new Viewer("User3", 30, user3Movies),
+                new Viewer("User4", 22, user4Movies),
+                new Viewer("User5", 27, user5Movies)
         );
 
         double averageAge = ViewerStatistics.averageAge(viewers);
         System.out.println("Средний возраст пользователей: " + averageAge);
+
+        // Пример вывода информации о фильмах пользователей
+        for (Viewer viewer : viewers) {
+            System.out.println(viewer.getNickname() + " просмотрел " + viewer.getMoviesWatchedCount() + " фильмов.");
+        }
     }
 }
