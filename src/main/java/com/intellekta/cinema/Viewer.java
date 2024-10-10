5d5aa711d2dc3a9e1a9e7f59159e296a778d80db
Korablev Danil <ikorablev@gmail.com>
2024-10-10 22:39:25 +0500

    replace integer watchedMoviesCount property to List of Cinema in Viewer class

--- src/main/java/com/intellekta/cinema/Viewer.java
+++ src/main/java/com/intellekta/cinema/Viewer.java
@@ -1,25 +1,31 @@
 package com.intellekta.cinema;
 
+import java.util.List;
+
 public class Viewer {
     private String nickname;
     private int age;
-    private int watchedMoviesCount;
+    private List<Cinema> watchedMovies;
 
-    public Viewer(String nickname, int age, int watchedMoviesCount) {
+    public Viewer(String nickname, int age, List<Cinema> watchedMovies) {
         this.nickname = nickname;
         this.age = age;
-        this.watchedMoviesCount = watchedMoviesCount;
-    }
-
-    public String getNickname() {
-        return nickname;
+        this.watchedMovies = watchedMovies;
     }
 
     public int getAge() {
         return age;
     }
 
-    public int getWatchedMoviesCount() {
-        return this.watchedMoviesCount;
+    public List<Cinema> getWatchedMovies() {
+        return watchedMovies;
+    }
+
+    public int getMoviesWatchedCount() {
+        return watchedMovies.size();
+    }
+
+    public String getNickname() {
+        return this.nickname;
     }
 }
